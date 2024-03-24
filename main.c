d6c2794a25a9efa68dd9a6ff67024698514f18cb
abvivek28 <abvivek28@gmail.com>
2024-03-25 00:40:34 +0530

    commit on 25mar24

--- main.c
+++ main.c
@@ -3,16 +3,31 @@
 
 int add_row(FILE *fp)
 {
-    char name[20];
-    int id;
-    int qty;
-    float price;
-    fp=fopen("warehouse.txt","a");
-    //check the order in which input has been taken
-    printf("enter name,id,quantity and price of each piece \n");
+    char name[20],name1[20];
+    int check=0;
+    int id,id1;
+    int qty,qty1;
+    float price,price1;
+    printf("Enter Name, ID, Quantity and Price of each piece: \n");
     scanf("%s %d %d %f",name,&id,&qty,&price);
-    fprintf(fp,"%d \t %s \t %d \t %f \n",id,name,qty,price);
+    fp=fopen("warehouse.txt","r");
+    while(fscanf(fp,"%d %s %d %f",&id1,name1,&qty1,&price1)==4)
+    {
+        if(id1==id)
+        {
+            printf("Unable to add item(ID already exists)\n");
+            check=1;
+            break;
+        }
+    }
     fclose(fp);
+    if(check==0)
+    {
+        fp=fopen("warehouse.txt","a");
+        fprintf(fp,"%d \t %s \t %d \t %f \n",id,name,qty,price);
+        fclose(fp);
+        printf("Row added\n");
+    }
     return 0;
 }
 
@@ -29,20 +44,20 @@ int get_details(FILE *fp)
         printf("Database not found\n");
         return 0;
     }
-    printf("enter id to be found\n");
+    printf("Enter ID to be found\n");
     scanf("%d",&to_find);
     while(fscanf(fp,"%d %s %d %f",&id,name,&qty,&price)==4)
     {
         if(id==to_find)
         {
-            printf("id :%d  product name:%s  quantity:%d  price is:%f\n",id,name,qty,price);
+            printf("ID :%d  Product Name:%s  Quantity:%d  Price is:%f\n",id,name,qty,price);
             check++;
             break;
         }
     }
     if(check==0)
     {
-        printf("product not found\n");
+        printf("Product not found\n");
     }
     fclose(fp);
     return 0;
@@ -60,9 +75,10 @@ int display_all(FILE *fp)
         printf("Database not found\n");
         return 0;
     }
-    while(fscanf(fp,"%d %s %d %f",&id,name,&qty,&price)==4)
+    printf("%-5s%-20s%-10s%-10s\n", "ID", "Product Name", "Quantity", "Price");
+    while (fscanf(fp, "%d %s %d %f", &id, name, &qty, &price) == 4)
     {
-        printf("id :%d  product name:%s  quantity:%d  price is:%f\n",id,name,qty,price);
+        printf("%-5d%-20s%-10d%-10.2f\n", id, name, qty, price);
     }
     fclose(fp);
     return 0;
@@ -83,12 +99,14 @@ int remove_row(FILE *fp)
         printf("Database not found\n");
         return 0;
     }
-    printf("enter id to be removed\n");
+    printf("Enter ID to be removed\n");
     scanf("%d",&to_find);
     while(fscanf(fp,"%d %s %d %f",&id,name,&qty,&price)==4)
     {
         if(id==to_find)
         {
+            printf("Product removed\n");
+            check=1;
             continue;
         }
         fprintf(fp1,"%d \t %s \t %d \t %f \n",id,name,qty,price);
@@ -97,6 +115,10 @@ int remove_row(FILE *fp)
     fclose(fp1);
     remove("warehouse.txt");
     rename("duplicate.txt","warehouse.txt");
+    if(check==0)
+    {
+        printf("Product not in warehouse(was not removed)\n");
+    }
     return 0;
 }
 
@@ -115,22 +137,66 @@ int sell_item(FILE *fp)
         printf("Database not found\n");
         return 0;
     }
-    printf("enter id of item sold\n");
+    printf("Enter ID of item to be sold:\n");
     scanf("%d",&to_find);
-    printf("enter quantity sold\n");
+    printf("Enter Quantity to be sold:\n");
     scanf("%d",&qty_sold);
     while(fscanf(fp,"%d %s %d %f",&id,name,&qty,&price)==4)
     {
         if(id==to_find)
         {
-            if(qty>=qty_sold)
+            if(qty>=qty_sold && qty_sold>=0)
             {
                 fprintf(fp1,"%d \t %s \t %d \t %f \n",id,name,(qty-qty_sold),price);
+                printf("Sale Processed\n");
                 continue;
             }
             else
             {
-                printf("there are not sufficient items to sell\n");
+                printf("Sale not processed(insufficient items to sell)\n");
+            }
+        }
+        fprintf(fp1,"%d \t %s \t %d \t %f \n",id,name,qty,price);
+    }
+    fclose(fp);
+    fclose(fp1);
+    remove("warehouse.txt");
+    rename("duplicate.txt","warehouse.txt");
+    return 0;
+}
+
+int restock_item(FILE *fp)
+{
+    FILE *fp1;
+    int id,to_find;
+    int qty,qty_add;
+    int check=0;
+    float price;
+    char name[20];
+    fp1=fopen("duplicate.txt","w");
+    fp=fopen("warehouse.txt","r");
+    if(fp==NULL)
+    {
+        printf("Database not found\n");
+        return 0;
+    }
+    printf("Enter ID of item restocked\n");
+    scanf("%d",&to_find);
+    printf("Enter quantity to be added\n");
+    scanf("%d",&qty_add);
+    while(fscanf(fp,"%d %s %d %f",&id,name,&qty,&price)==4)
+    {
+        if(id==to_find)
+        {
+            if(qty_add>0)
+            {
+                fprintf(fp1,"%d \t %s \t %d \t %f \n",id,name,(qty+qty_add),price);
+                printf("Restocking successful\n");
+                continue;
+            }
+            else
+            {
+                printf("Unable to Restock(Invalid Input)\n");
             }
         }
         fprintf(fp1,"%d \t %s \t %d \t %f \n",id,name,qty,price);
@@ -149,7 +215,7 @@ int main()
     int dec;
     while(i)
     {
-       printf("Enter 1 to add row\nEnter 2 to display row\nEnter 3 to display complete database\nEnter 4 to remove a row\nEnter 5 to sell item\nAny other key to exit program\n");
+       printf("Enter 1 to add an item\nEnter 2 to display item\nEnter 3 to display warehouse stock\nEnter 4 to remove an item\nEnter 5 to sell an item\nEnter 6 to restock\nAny other key to exit program\n");
        scanf("%d",&dec);
        switch(dec)
        {
@@ -167,6 +233,9 @@ int main()
                break;
            case 5:
                sell_item(fp);
+               break;
+           case 6:
+               restock_item(fp);
                break;
            default:
                printf("Thank You");
